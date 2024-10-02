3edb237174e1c591927f96efa5da1d2bfed00416
LuchoCruz08 <lucianovcruz2004@gmail.com>
2024-10-02 12:34:57 -0300

    First commit

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { cn } from '@/lib/utils'
+import { FileText } from "lucide-react";
+
+const Header = ({ children, className }: HeaderProps) => {
+  return (
+    <div className={cn("header", className)}>
+      <Link href="/" className="md:flex-1">
+        <FileText
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
+        <FileText
+          width={32}
+          height={32}
+          className="mr-2 md:hidden"
+        />
+      </Link>
+      {children}
+    </div>
+  );
+};
+
+export default Header;
